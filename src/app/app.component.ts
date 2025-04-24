62308b6bdbebbf920d9d82eb1051f53b30eddbc1
Karthick-sketch <karthick246.kh@outlook.com>
2025-04-24 20:11:52 +0000

    Add skeleton Diet Planner page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DietPlannerComponent } from './diet-planner/diet-planner.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [DietPlannerComponent],
 })
 export class AppComponent {}
---
+++ src/app/diet-planner/diet-planner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diet-planner',
+  templateUrl: './diet-planner.component.html',
+  styleUrl: './diet-planner.component.css',
+})
+export class DietPlannerComponent {}
