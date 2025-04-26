58195e7760e4b2e78ea7af6205bbcf837300d181
Karthick-sketch <karthick246.kh@outlook.com>
2025-04-26 20:37:34 +0000

    Add routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DietPlannerComponent } from './diet-planner/diet-planner.component';
+import { DietPlanFormComponent } from './diet-planner/diet-plan-form/diet-plan-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/diet-planner',
+    pathMatch: 'full',
+  },
+  {
+    path: 'diet-planner',
+    component: DietPlannerComponent,
+  },
+  {
+    path: 'diet-plan-form',
+    component: DietPlanFormComponent,
+  },
+];
--- src/app/diet-planner/diet-planner.component.ts
+++ src/app/diet-planner/diet-planner.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-diet-planner',
   templateUrl: './diet-planner.component.html',
   styleUrl: './diet-planner.component.css',
 })
-export class DietPlannerComponent {}
+export class DietPlannerComponent {
+  constructor(private router: Router) {}
+
+  openDietPlanForm() {
+    this.router.navigate(['/diet-plan-form']);
+  }
+}
