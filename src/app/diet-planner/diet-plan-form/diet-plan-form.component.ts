efd5549c9004a6571e8e934e1ec55ccbc045ae54
Karthick-sketch <karthick246.kh@outlook.com>
2025-05-20 22:45:11 +0530

    Add svg files

---
+++ src/app/diet-planner/diet-plan-form/diet-plan-form.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-diet-plan-form',
+  templateUrl: './diet-plan-form.component.html',
+  styleUrl: './diet-plan-form.component.css'
+})
+export class DietPlanFormComponent {
+
+}
