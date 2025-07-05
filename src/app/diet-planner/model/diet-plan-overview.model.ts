7e0c43b359f9c026d37039b5d69009aa1ecfd305
Karthick-sketch <karthick246.kh@outlook.com>
2025-07-05 13:22:57 +0530

    Redesign Daily Track entries history page

--- src/app/diet-planner/model/diet-plan-overview.model.ts
+++ src/app/diet-planner/model/diet-plan-overview.model.ts
@@ -3,6 +3,7 @@ import { CaloriesDTO } from '../dto/calories.dto';
 export class DietPlanOverviewModel {
   id!: string;
   weight!: number;
+  tdee!: number;
   deficit!: CaloriesDTO;
   protein!: CaloriesDTO;
   carbs!: CaloriesDTO;
